e081367bdad7912edf0c872c4245aee7fa4312b8
wuhencut <1076668563@qq.com>
2019-07-03 17:19:52 +0800

    + login router + api

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
+import router from './router'
+import './assets/css/base.less'
+import api from './api/index'
 
 Vue.config.productionTip = false
+Vue.prototype.$api = api
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page">
+    <span>1111</span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return {
+
+    }
+  },
+  mounted(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="less" scoped>
+.page{
+  color: #000;
+}
+</style>
